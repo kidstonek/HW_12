8d28f65734f2061625d1c4f82b3edd4d6d3ce3a5
Andrii V <kidstonek@gmail.com>
2022-06-09 21:51:22 +0300

    HW 12 _ AP 1-2

---
+++ AP/HW_12_ex_02.py
@@ -0,0 +1,68 @@
+"""
+Есть список, каждый элемент которого — это словарь с контактами пользователя следующего вида:
+
+{
+    "name": "Allen Raymond",
+    "email": "nulla.ante@vestibul.co.uk",
+    "phone": "(992) 914-3792",
+    "favorite": False,
+}
+Словарь содержит имя пользователя name, его email, телефонный номер phone и свойство favorite — избранный контакт
+или нет.
+
+Разработайте две функции для сериализации и десериализации списка контактов с помощью пакета json и
+хранения полученных данных в текстовом файле.
+
+Первая функция write_contacts_to_file принимает два параметра: filename — имя файла, contacts — список контактов.
+Она сохраняет указанный список в файл, используя метод dump пакета json.
+
+Структура json файла должна быть следующей:
+
+{
+  "contacts": [
+    {
+      "name": "Allen Raymond",
+      "email": "nulla.ante@vestibul.co.uk",
+      "phone": "(992) 914-3792",
+      "favorite": false
+    },
+    ...
+  ]
+}
+Т.е. список контактов должен храниться по ключу "contacts", а не просто сохранить его в файл.
+
+Вторая функция read_contacts_from_file читает и возвращает указанный список contacts из файла filename,
+сохраненного ранее функцией write_contacts_to_file, используя метод load пакета json.
+"""
+
+
+import json
+
+
+def write_contacts_to_file(filename, contacts):
+    with open(filename, "w") as f:
+        bb = {'contacts': contacts}
+        json.dump(bb, f)
+
+
+def read_contacts_from_file(filename):
+    with open(filename, "r") as f:
+        unpack = json.load(f)
+    return unpack['contacts']
+
+
+def main():
+    asd = {
+        "name": "Allen Raymond",
+        "email": "nulla.ante@vestibul.co.uk",
+        "phone": "(992) 914-3792",
+        "favorite": False,
+    }
+
+    filename = 'files/hw_12_02.bin'
+    write_contacts_to_file(filename, asd)
+    print(read_contacts_from_file(filename))
+
+
+if '__main__' == __name__:
+    main()
